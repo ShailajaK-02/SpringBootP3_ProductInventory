a8726d4a5c9d27d493ca32ff2f2ae7cb28702d82
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-06 13:37:23 +0530

    entity class added

--- ProductInventoryManager/src/main/java/com/inventory/entity/ProductDetails.java
+++ ProductInventoryManager/src/main/java/com/inventory/entity/ProductDetails.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+//entity class
 @Entity
 public class ProductDetails {
     @Id
