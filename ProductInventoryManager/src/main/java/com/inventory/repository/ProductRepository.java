3680e9a6893e55da7cd7eb92a07041e5066b60fa
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-06 13:37:40 +0530

    repository interface added

--- ProductInventoryManager/src/main/java/com/inventory/repository/ProductRepository.java
+++ ProductInventoryManager/src/main/java/com/inventory/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.inventory.repository;
 import com.inventory.entity.ProductDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Repository interface
 public interface ProductRepository extends JpaRepository<ProductDetails,Integer>
 {
 }
